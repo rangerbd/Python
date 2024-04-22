4f6b0a4011a5799d0f797a37e44eb9cfa6fc6550
rangerbd <32087004+rangerbd@users.noreply.github.com>
2024-04-22 23:48:00 +0200

    Add files via upload

---
+++ day 3/main.py
@@ -0,0 +1,12 @@
+import random
+import my_module
+
+random_nunber = random.randint(1,10)+1
+print(random_nunber)
+
+#create random float number with a range
+random_float = random.uniform(0,5)
+print(random_float)
+
+
+print(my_module.name)
